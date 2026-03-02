1b5f3abab7fed51831919b948600fdb79e912c2d
MohammadJewelRana <js.rana0326@gmail.com>
2026-03-02 20:09:43 +0600

    categories designed

--- quickHire-client/app/(MainLayout)/(home)/page.tsx
+++ quickHire-client/app/(MainLayout)/(home)/page.tsx
@@ -1,10 +1,16 @@
+import Categories from "./_components/Catrgories/Category";
+import Companies from "./_components/Companies";
 import Hero from "./_components/Hero";
- 
 
 const page = () => {
   return (
     <div>
       <Hero />
+
+      <div className="  bg-white">
+        <Companies />
+        <Categories/>
+      </div>
     </div>
   );
 };
